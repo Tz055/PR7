7037ea5c71a23c845e368e05eee1e4959531dda6
Tz055 <zzhilyaev@Gmail.com>
2025-12-17 01:02:05 +0300

    Initial commit

--- index.py
+++ index.py
@@ -1,14 +1,22 @@
-def main():
-    # Ввод данных
-    numbers = input("Введите числа через пробел: ").split()
-    
-    # Преобразование в int, удаление повторов и сортировка
-    unique_sorted_numbers = sorted(set(map(int, numbers)))
-    
-    # Вывод результата
+def find_unique_numbers(input_numbers):
+    """
+    Функция принимает список целых чисел
+    и возвращает отсортированный список уникальных чисел.
+    """
+    unique_numbers = []
+
+    for number in input_numbers:
+        if number not in unique_numbers:
+            unique_numbers.append(number)
+
+    # Сортировка списка уникальных чисел
+    unique_numbers.sort()
+    return unique_numbers
+
+try:
+    numbers = list(map(int, input("Введите числа через пробел: ").split()))
+    result = find_unique_numbers(numbers)
     print("Уникальные числа в порядке возрастания:")
-    print(*unique_sorted_numbers)
-
-
-if __name__ == "__main__":
-    main()
+    print(result)
+except ValueError:
+    print("Ошибка: введите только целые числа.")
